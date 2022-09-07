238ace53409d47acd26478ebe2129eaac65207ca
mulat <mulatmek@gmail.com>
2022-09-07 17:47:03 +0300

    added food collision ,food class

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import time
 from snack import Snack
-from turtle import Screen, Turtle
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("my Snake Game")
-screen.tracer(0)
+from turtle import Screen
+from food import Food
 
-snack =Snack()
-# listen to events
-screen.listen()
-screen.onkey(snack.up,"Up")
-screen.onkey(snack.down,"Down")
-screen.onkey(snack.left,"Left")
-screen.onkey(snack.right,"Right")
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snack.move()
 
-screen.exitonclick()
+def setting_keys(screen, snack):
+    screen.onkey(snack.up, "Up")
+    screen.onkey(snack.down, "Down")
+    screen.onkey(snack.left, "Left")
+    screen.onkey(snack.right, "Right")
+
+
+def main():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("my Snake Game")
+    screen.tracer(0)
+
+    snack = Snack()
+    food = Food()
+
+    # listen to events
+    screen.listen()
+    setting_keys(screen, snack)
+
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snack.move()
+        if snack.head.distance(food) < 15:
+            food.refresh()
+        # collision with food
+
+    screen.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
--- snack.py
+++ snack.py
@@ -1,6 +1,4 @@
-import time
-import turtle
-from turtle import Screen, Turtle
+from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
